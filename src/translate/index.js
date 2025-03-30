afaeeb6748a1cbbf83dd07d2c139aa0df3d4c895
ameliakratzer-code <akratzer@andrew.cmu.edu>
2025-03-30 16:54:16 -0400

    Edited translator URL

--- src/translate/index.js
+++ src/translate/index.js
@@ -3,8 +3,7 @@ var request = require('request');
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-    // Edit the translator URL below
-    const TRANSLATOR_API = "http://translator:5000/"
+    const TRANSLATOR_API = "http://127.0.0.1:5000"
     const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
     const data = await response.json();
     console.log(data)
