ac17f15c0a3389ee482d2bc9c6e80beff86bdf82
ameliakratzer-code <akratzer@andrew.cmu.edu>
2025-04-01 15:04:13 -0400

    Changed translator API

--- src/translate/index.js
+++ src/translate/index.js
@@ -3,7 +3,8 @@
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-    const TRANSLATOR_API = "http://127.0.0.1:5000"
+    //"http://127.0.0.1:5000"
+    const TRANSLATOR_API = "http://translator:5000/"
     try {
         const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
         if (!response.ok) {
