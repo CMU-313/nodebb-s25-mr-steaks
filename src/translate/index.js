31998606a9e9184efb949359eaac89dda72bfaeb
ameliakratzer-code <akratzer@andrew.cmu.edu>
2025-04-01 14:27:32 -0400

    Added catch error

--- src/translate/index.js
+++ src/translate/index.js
@@ -4,8 +4,17 @@ const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
     const TRANSLATOR_API = "http://127.0.0.1:5000"
-    const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
-    const data = await response.json();
-    console.log(data)
-    return [data["is_english"], data["translated_content"]]
+    try {
+        const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data)
+        return [data["is_english"], data["translated_content"]]
+    }
+    catch (error) {
+        console.error("Translation fetch failed:", error);
+        return [false, postData.content];
+    }
 }
