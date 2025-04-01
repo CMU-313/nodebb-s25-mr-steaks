1c4044823b1d3d8cbe46ddfdafe743dec1312866
ameliakratzer-code <akratzer@andrew.cmu.edu>
2025-04-01 15:10:14 -0400

    Delete slash

--- src/translate/index.js
+++ src/translate/index.js
@@ -6,7 +6,7 @@ translatorApi.translate = async function (postData) {
     // "http://127.0.0.1:5000"
     const TRANSLATOR_API = "http://translator:5000/"
     try {
-        const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
+        const response = await fetch(`${TRANSLATOR_API}?content=${postData.content}`);
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
