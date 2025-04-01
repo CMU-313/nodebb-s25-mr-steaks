22d5a7df4992779247ab56e983e57d3609d4268a
ameliakratzer-code <akratzer@andrew.cmu.edu>
2025-04-01 14:58:52 -0400

    Style changes

--- src/translate/index.js
+++ src/translate/index.js
@@ -1,17 +1,17 @@
-var request = require('request');
+'use strict';
 
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
     const TRANSLATOR_API = "http://127.0.0.1:5000"
     try {
-        const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
+        const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const data = await response.json();
         console.log(data)
-        return [data["is_english"], data["translated_content"]]
+        return [data.is_english, data.translated_content]
     }
     catch (error) {
         console.error("Translation fetch failed:", error);
